00509a6546bc311dae3a633a9781733bc8755a02
Markus Klotzbuecher <mk@mkio.de>
2017-08-29 09:29:00 +0200

    initial version of ipk serving tool

---
+++ ipk-donkey.scm
@@ -0,0 +1,35 @@
+;;
+;; Minimal yocto ipk package server
+;;
+
+(use spiffy fmt fmt-color posix irregex
+     simple-directory-handler
+     simple-configuration)
+
+(define (write-default-dot-file fn)
+  (with-output-to-file fn
+    (lambda () (fmt #t
+		    "((server-port 8080)" nl
+		    " (logging-enabled #t))" nl))))
+
+(let* ((build-dir (get-environment-variable "BUILDDIR"))
+       (ipk-root (string-append build-dir "/tmp/deploy/ipk"))
+       (dot-file (normalize-pathname "~/.ipk-donkey")))
+  (when (eq? build-dir #f)
+    (fmt #t (fmt-red "error: BUILDDIR environment variable not set") nl)
+    (exit 1))
+  (unless (directory-exists? ipk-root)
+    (fmt #t (fmt-red "error: root-path not a directory: " ipk-root) nl)
+    (exit 2))
+  (unless (file-exists? dot-file)
+    (write-default-dot-file dot-file))
+  (let ((cfg (config-read dot-file eval-config: #t)))
+    (parameterize
+	((root-path ipk-root)
+	 (server-port (config-ref cfg '(server-port) default: 80))
+	 (handle-directory simple-directory-handler)
+	 (error-log (current-error-port))
+	 (access-log (if (config-ref cfg '(logging-enabled))
+			 (current-output-port) #f)))
+      (fmt #t "serving directory " (fmt-green (root-path)) " on port " (fmt-green (server-port)) nl)
+      (start-server))))
