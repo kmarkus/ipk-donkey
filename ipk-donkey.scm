df0c081eccc5853926b9b8dd154e91e92c551179
Markus Klotzbuecher <mk@mkio.de>
2020-03-27 08:46:56 +0100

    port to Chicken 5
    this replaces the config file with simple cmdline args.

--- ipk-donkey.scm
+++ ipk-donkey.scm
@@ -2,35 +2,59 @@
 ;; Minimal yocto ipk package server
 ;;
 
-(use spiffy fmt fmt-color posix irregex
-     simple-directory-handler
-     simple-configuration
-     pathname-expand)
+(import (chicken file)
+	(chicken irregex)
+	(chicken pathname)
+	(chicken process-context)
+	(chicken condition)
+	spiffy
+	fmt fmt-color
+	srfi-98
+	simple-directory-handler
+	pathname-expand
+	getopt-long)
 
-(define (write-default-dot-file fn)
-  (with-output-to-file fn
-    (lambda () (fmt #t
-		    "((server-port 8080)" nl
-		    " (logging-enabled #t))" nl))))
+(define (absolutize-path path)
+  (let ((cwd (current-directory)))
+    (if (absolute-pathname? path)
+        (normalize-pathname path)
+        (normalize-pathname (conc cwd "/" path)))))
 
-(let* ((build-dir (get-environment-variable "BUILDDIR")))
-  (when (eq? build-dir #f)
-	(fmt #t (fmt-red "error: BUILDDIR environment variable not set") nl)
-	(exit 1))
-  (let* ((ipk-root (string-append build-dir "/tmp/deploy/ipk"))
-	 (dot-file (pathname-expand "~/.ipk-donkey")))
-    (unless (directory-exists? ipk-root)
-	    (fmt #t (fmt-red "error: root-path not a directory: " ipk-root) nl)
-	    (exit 2))
-    (unless (file-exists? dot-file)
-	    (write-default-dot-file dot-file))
-    (let ((cfg (config-read dot-file eval-config: #t)))
-      (parameterize
-       ((root-path ipk-root)
-	(server-port (config-ref cfg '(server-port) default: 80))
-	(handle-directory simple-directory-handler)
-	(error-log (current-error-port))
-	(access-log (if (config-ref cfg '(logging-enabled))
-			(current-output-port) #f)))
-       (fmt #t "serving directory " (fmt-green (root-path)) " on port " (fmt-green (server-port)) nl)
-       (start-server)))))
+(define grammar
+  `((port "port on which to serve the ipks"
+	  (required #f)
+	  (value #t)
+	  (single-char #\p)
+	  (value (required PORT)
+		 (transformer ,string->number)))
+
+    (verbose "enable verbose output" (single-char #\v))
+    (help    "print this"            (single-char #\h))))
+
+(define (usage-and-exit code)
+  (fmt #t "Usage: " (car (argv)) " [options...]" nl)
+  (fmt #t (usage grammar))
+  (exit code))
+
+(with-exception-handler
+ (lambda (exn) (print-error-message exn) (exit 1))
+ (lambda ()
+   (let* ((build-dir (get-environment-variable "BUILDDIR"))
+	  (opts (getopt-long (command-line-arguments) grammar)))
+     (when (alist-ref 'help opts)
+       (usage-and-exit 0))
+     (when (eq? build-dir #f)
+       (fmt #t (fmt-red "error: BUILDDIR environment variable not set") nl)
+       (usage-and-exit 1))
+     (let* ((ipk-root (string-append build-dir "/tmp/deploy/ipk")))
+       (unless (directory-exists? ipk-root)
+	 (fmt #t (fmt-red "error: root-path not a directory: " ipk-root) nl)
+	 (exit 2))
+       (parameterize
+	   ((root-path ipk-root)
+	    (server-port (or (alist-ref 'port opts) 8080))
+	    (handle-directory simple-directory-handler)
+	    (error-log (current-error-port))
+	    (access-log (if (alist-ref 'verbose opts) (current-output-port) #f)))
+	 (fmt #t "serving directory " (fmt-green (root-path)) " on port " (fmt-green (server-port)) nl)
+	 (start-server))))))
